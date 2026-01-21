febbb8af3bfd54fc985029be8322ae9c9c89f6ee
343306 <01206747@pw.edu.pl>
2026-01-21 18:18:29 +0100

    Refactor argument handling and add parameters

--- src/main.c
+++ src/main.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <string.h>
+#include "pso.h"
 #include "map.h"
-
-/*
-Puki co odpalanie i obsluga bledow
-
-Zwraca 0 - Gdy program zakonczyl dzialanie sukcesem
-Zwraca 1 - Gdy nie podano zadnych argumentow wywolania
-Zwraca 2 - Gdy podano nieparzysta liczbe argumentow
-Zwraca 3 - Nie udalo sie wczytac mapy
-*/
 
 int main(int argc, char** argv)
 {
@@ -20,13 +12,40 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    if(argc%2 == 1){
-        fprintf(stderr,"[%s] Error 2: Niewlasciwa strukrura argumentow.\n",argv[0]);
-        return 2;
-    }
-
     srand(time(NULL));
 
+    double w = 0.5, c1 = 1.0, c2 = 1.0, r1 = ((double)rand()) / RAND_MAX, r2 = ((double)rand()) / RAND_MAX;
+    int p = 30, n = 0, i = 100, j;
+    FILE* c = NULL;
+
+    for (j = 1; j < argc; j++) {
+        if (strcmp(argv[j], "-p") == 0) {
+            j++;
+            if (j < argc)
+                if(sscanf(argv[j], "%d", &p) != 1) {}
+        }
+        if (strcmp(argv[j], "-i") == 0) {
+            j++;
+            if (j < argc)
+                if(sscanf(argv[j], "%d", &i) != 1) {}
+        }
+        if (strcmp(argv[j], "-n") == 0) {
+            j++;
+            if (j < argc)
+                if(sscanf(argv[j], "%d", &n) != 1) {}
+        }
+        if (strcmp(argv[j], "-c") == 0) {
+            j++;
+            if (j < argc)
+                c = fopen(argv[j], "r");
+            if (c != NULL) {
+                if (fscanf(c, "%lf %lf %lf %lf %lf", &w, &c1, &c2, &r1, &r2) != 5) {}
+			    fclose(c);
+            }
+        }
+    }
+
+    Swarm* CreateSwarm(p);
     Map *map = LoadMap(argv[1]);
 
     if(map == NULL){
