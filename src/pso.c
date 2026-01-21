22ddd97859f53f5d4d3f39c6fc16f9dcbfd4e6ae
343306 <01206747@pw.edu.pl>
2026-01-21 18:19:15 +0100

    Add swarm and particle creation functions
    Implement functions to create and free a swarm of particles.

--- src/pso.c
+++ src/pso.c
@@ -1,1 +1,57 @@
 #include "pso.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "utilis.h"
+
+Swarm* CreateSwarm(int p) {
+
+    Swarm* swarm = (Swarm*)malloc(sizeof(Swarm));
+
+    if (swarm != NULL) {
+        swarm->particles = (Particle**)malloc(p * sizeof(Particle*));
+        swarm->gbest = (double*)malloc(2 * sizeof(double));
+        swarm->p = p;
+
+        if (swarm->particles != NULL) {
+            for (int i = 0; i < p; i++) {
+                swarm->particles[i] = CreateParticle();
+            }
+        }
+    }
+    return swarm;
+}
+
+Particle* CreateParticle() {
+
+	Particle* particle = (Particle*)malloc(sizeof(Particle));
+
+    if (particle != NULL) {
+        particle->x = (double*)malloc(2 * sizeof(double));
+        particle->v = (double*)malloc(2 * sizeof(double));
+        particle->pbest = (double*)malloc(2 * sizeof(double));
+    }
+
+    particle->x[0] = (double)(rand() % 7);
+    particle->x[1] = (double)(rand() % 7);
+	particle->v[0] = RandDouble(-1.0, 1.0);
+	particle->v[1] = RandDouble(-1.0, 1.0); 
+	particle->pbest[0] = particle->x[0];
+	particle->pbest[1] = particle->x[1];
+    return particle;
+}
+
+void FreeSwarm(Swarm* swarm) {
+    if (!swarm) return;
+
+    for (int i = 0; i < swarm->p; i++) {
+        free(swarm->particles[i]->x);
+        free(swarm->particles[i]->v);
+        free(swarm->particles[i]->pbest);
+        free(swarm->particles[i]);
+    }
+    free(swarm->particles);
+    free(swarm->gbest);
+    free(swarm);
+}
+
+
