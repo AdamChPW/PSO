1d5db49b91d5647b79b036cf815dea6f169d5412
343306 <01206747@pw.edu.pl>
2026-01-22 18:15:45 +0100

    Update logger.c

--- src/logger.c
+++ src/logger.c
@@ -4,6 +4,6 @@
 void LogPosition(FILE *f, int i, int x, int y, double value)
 {
 	if (f != NULL) {
-		fprintf(f, "CZĄSTKA %d: [%d]	[%d] : %lf\n", i, x, y, value);
+		fprintf(f, "CZĄSTKA %d: [%d]	[%d] : %.2lf\n", i, x, y, value);
 	}
 }
